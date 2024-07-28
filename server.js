789fad046c74d2848e7ee7209b02740edc0013f7
Abhay-Rana-18 <ranaabhay795@gmail.com>
2024-07-29 00:25:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData?._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined Room: " + room);
+  });
+
+  socket.on("typing", (room, msg) => {
+    socket.in(room).emit("typing", msg);
+    console.log(msg);
+  });
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (msg) => {
+    const chat = msg.chat;
+    if (!chat?.users) {
+      return console.log("Chat users not defined.");
+    }
+
+    chat.users.forEach((user) => {
+      if (user === msg.sender) return;
+      console.log("message sending to: " + user + ", msg: " + msg?.content);
+      io.to(user).emit("message received", msg);
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+console.log("server");
